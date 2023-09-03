de93ff74d1896a8c21337e08aedefc5eccb2ebdf
Alvaro Llorente <alvaro.llorente@lisdatasolutions.com>
2023-09-03 12:41:15 +0200

    DEV-Correct point click

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useRef, useCallback, useState } from 'react'
+import { useRef, useCallback, useState, useEffect } from 'react'
 import { Layer, Map, MapLayerMouseEvent, MapRef, Source } from 'react-map-gl'
 import { FOG, INITIAL_VIEW, MAP_DARK } from './constants/map'
 import { heatmapLayer, pointLayer, pulsingDot } from './components/Layers'
@@ -9,19 +9,29 @@ import { HovInfo } from './interface/map'
 import { Filter } from './components/FilterTime'
 import styles from './styles/main.module.css'
 import { FilterMagnitude } from './components/FilterMagnitude'
+import { mappedEarthQuake } from './helpers/mappedData'
 
 export const App = () => {
   const mapRef = useRef<MapRef>()
   const [hoverInfo, setHoverInfo] = useState<HovInfo>()
 
   const handleResetZoom = () => mapRef.current?.flyTo(INITIAL_VIEW)
-  const onClick = ({ lngLat, features }: Partial<MapLayerMouseEvent>) => {
-    features!.length > 0 && mapRef.current?.flyTo({ center: lngLat, zoom: 6 })
+  const onClick = ({ features }: Partial<MapLayerMouseEvent>) => {
+    const coord = JSON.parse(features![0].properties!.coordinates)
+    features!.length > 0 && mapRef.current?.flyTo({ center: coord, zoom: 6 })
   }
   const onHover = useCallback(({ features, point }: MapLayerMouseEvent) => {
     const { x, y } = point
     const hoveredFeature = features && features[0]
     setHoverInfo(hoveredFeature && { feature: hoveredFeature, x, y })
+  }, [])
+
+  const [data, setData] = useState<any>()
+
+  useEffect(() => {
+    mappedEarthQuake({
+      url: 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson'
+    }).then(setData)
   }, [])
 
   return (
@@ -40,7 +50,7 @@ export const App = () => {
         onClick={onClick}
         fog={FOG}
       >
-        <Source type="geojson" data={EARTHQUAKE_TIME.PAST_DAY.ALL.VALUE}>
+        <Source type="geojson" data={data}>
           <Layer {...pointLayer} id="wave"></Layer>
         </Source>
         <Source type="geojson" data={EARTHQUAKE_TIME.PAST_DAY.ALL.VALUE}>
--- src/helpers/mappedData.ts
+++ src/helpers/mappedData.ts
@@ -1,23 +1,26 @@
-import { EarthQuakes, Feature } from '../interface/earthQuakes'
+import { Feature } from '../interface/earthQuakes'
 import { detailUrlService } from '../services/api'
 
 export const mappedEarthQuake = async ({ url }: { url: string }) => {
   const data = await detailUrlService(url)
-  const mappedData = data.map((earthQuake: EarthQuakes) => {
+  const mappedData = data.features.map((earthQuake: Feature) => {
     return {
-      type: 'FeatureCollection',
-      features: earthQuake.features.map((feat: Feature) => {
-        return {
-          type: feat.type,
-          geometry: feat.geometry,
-          properties: {
-            magnitude: feat.properties.mag,
-            place: feat.properties.place,
-            time: feat.properties.time
-          }
+      type: earthQuake.type,
+      geometry: earthQuake.geometry,
+      properties: {
+        magnitude: earthQuake.properties.mag,
+        tsunami: earthQuake.properties.tsunami,
+        place: earthQuake.properties.place,
+        time: earthQuake.properties.time,
+        coordinates: {
+          lng: earthQuake.geometry.coordinates[0],
+          lat: earthQuake.geometry.coordinates[1]
         }
-      })
+      }
     }
   })
-  return mappedData
+  return {
+    type: 'FeatureCollection',
+    features: mappedData
+  }
 }
